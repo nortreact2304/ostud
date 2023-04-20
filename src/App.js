6711d863550168f9007421007c52b3c9dce86215
Andres Järviste <andres.jarviste@wbscodingschool.com>
2023-04-20 19:42:26 +0300

    loeng5

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Ostud from './Ostud'
 
 function App() {
   const [ostukorv, setOstukorv] = useState([
-    { nimetus: 'pirnid', kogus: 1, korvis: false },
-    { nimetus: 'leib', kogus: 1, korvis: false },
+    { nimetus: 'pirnid', kogus: 1, yhik: 'kg', korvis: true },
+    { nimetus: 'leib', kogus: 1, yhik: 'tk', korvis: false },
   ])
 
   const [toode, setToode] = useState('')
+  const [kogus, setKogus] = useState(1)
+  const [yhik, setYhik] = useState('tk')
 
-  const lisaOstukorvi = (nimetus, kogus) => {
+  const lisaOstukorvi = () => {
     const uusToode = {
-      nimetus: nimetus, 
-      kogus: kogus, 
+      nimetus: toode, 
+      kogus: kogus,
+      yhik: yhik,
       korvis: false
     }
     //ostukorv.push(uusToode) <- nii ei saa lisada et reaktiivsus säliks
@@ -21,8 +24,10 @@ function App() {
   }
 
   const lisaKlikk = () => {  
-    lisaOstukorvi(toode, 1)
+    lisaOstukorvi()
     setToode('')
+    setKogus(1)
+    setYhik('tk')
     console.log(ostukorv)
   }
 
@@ -30,12 +35,54 @@ function App() {
     setToode(event.target.value)
   }
 
+  const kustutaToode = (index) => {
+    console.log('Kustutame toodet ' + index)
+    const uusMassiiv = ostukorv.filter( (ost, i) => {
+      return index !== i
+    } )
+    setOstukorv(uusMassiiv)
+  }
+
+  const toggleKasKorvis = (index) => {
+    const uusMassiiv = ostukorv.map((ost, i) => {
+      if (i != index) {
+        return ost
+      } else {
+        ost.korvis = !ost.korvis
+        return {...ost}
+      }
+    })
+
+    setOstukorv(uusMassiiv)
+    console.log(uusMassiiv)
+  }
+
   return (
     <div className="container">
-      <input value={toode} onChange={toodeMuutus} />
-      <button onClick={lisaKlikk}>Lisa</button>
+      <div className='row'>
+      <input className="col-4" value={toode} onChange={toodeMuutus} placeholder='Toote nimetus' />
+      <input
+        className='col-2' 
+        value={yhik} 
+        onChange={(event) => setYhik(event.target.value)}
+        placeholder="ühik" 
+      />
+      <input 
+         className='col-2'
+        value={kogus} 
+        onChange={(event) => setKogus(event.target.value)}
+        placeholder="kogus" 
+      />
+      <button className='col-3' onClick={lisaKlikk}>Lisa</button>
+
+      </div>
+      
       <h1>Ostukorv</h1>
-      <Ostud ostukorv={ostukorv} />
+      <Ostud 
+        ostukorv={ostukorv} 
+        kustutaToode={kustutaToode}
+        toggleKasKorvis = {toggleKasKorvis}
+      />
     </div>
   );
 }
--- src/Ostud.js
+++ src/Ostud.js
@@ -1,12 +1,36 @@
-function Ostud({ostukorv}) {
+function Ostud({ostukorv, kustutaToode, toggleKasKorvis}) {
+    const kustutaKlikk = (index) => {
+        kustutaToode(index)
+    }
+
+    //CRUD operats
+
+
     return (
         <div className="ostud">
             {
             ostukorv.map((ost, index) => {
+                let reaKlassid = "row"
+                if (ost.korvis) {
+                    reaKlassid += " korvis"
+                }
                 return (
-                    <div className="row" key={index}>
-                        <div className="col-7">{ost.nimetus}</div>
+                    <div className={reaKlassid} key={index}>
+                        <input 
+                            className="col-1" 
+                            type="checkbox"  
+                            defaultChecked={ost.korvis}
+                            onChange={() => toggleKasKorvis(index)}
+                        />
+                        <div className="col-5">{ost.nimetus}</div>
                         <div className="col-3">{ost.kogus}</div>
+                        <div className="col-2">{ost.yhik}</div>
+                        <button 
+                            className="col-1 btn btn-link"
+                            onClick={() => kustutaKlikk(index)}
+                        >
+                                X
+                        </button>
                     </div>
                 )
             })  
